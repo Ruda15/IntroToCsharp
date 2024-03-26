34b7903f7cb5dbed39ebdd0444eb5972eef5500e
ruda15 <rubendavtyan14@gmail.com>
2024-03-26 22:51:00 +0400

    Homwork

---
+++ Homework/Work2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Please input number");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine($"{7} and {23} is ktratno {num}");
+}
+else
+{
+    System.Console.WriteLine($"{7} and {23} is no ktratno {num}");
+}
